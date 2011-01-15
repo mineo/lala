57e78f3f4984667c727459e3671d7626caaee369
Wieland Hoffmann <themineo@gmail.com>
2011-01-15 18:37:22 +0100

    Update the start script

--- run-lala.py
+++ run-lala.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python2
 import lala
 import ConfigParser
+import sys
 
 def main():
     """Main method"""
     config = ConfigParser.SafeConfigParser()
     config.read("config")
     lalaconfig = config._sections["lala"]
+    if "-d" in sys.argv:
+        debug = True
+    else:
+        debug = False
     bot = lala.Bot(
             server=lalaconfig["server"],
             admin=lalaconfig["admin"],
             port=int(lalaconfig["port"]),
             nick=lalaconfig["nick"],
-            channel=lalaconfig["channel"]
+            channel=lalaconfig["channel"],
+            debug=debug
             )
-    try:
-        bot.mainloop()
-    except RuntimeError, e:
-        print e
+    #try:
+    bot.mainloop()
+    #except RuntimeError, e:
+        #print e
 
 if __name__ == '__main__':
     main()
