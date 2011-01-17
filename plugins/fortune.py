86667d26deddcb92ecab2084d775da1773363448
Wieland Hoffmann <themineo@gmail.com>
2011-01-17 16:30:42 +0100

    Adding a fortune plugin

---
+++ plugins/fortune.py
@@ -0,0 +1,16 @@
+import plugin
+import logging
+import subprocess
+
+class Plugin(plugin.baseplugin):
+    def __init__(self, bot):
+        bot.register_callback("fortune", self.fortune)
+
+    def fortune(self, bot, user, channel, text):
+        try:
+            p = subprocess.Popen(["fortune", "fortunes"],
+                    stdout=subprocess.PIPE)
+            result = p.communicate()[0]
+            bot.privmsg(channel,"%s: %s" % (user, result.replace("\n","")))
+        except OSError, e:
+            logging.error("Error while calling fortune: %s" % e)
