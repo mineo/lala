7b116efd50609a27ca3b3d5b095ae252cb082204
Wieland Hoffmann <themineo@gmail.com>
2011-02-01 13:14:13 +0100

    0.1

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name="lala",
+        version="0.1",
+        author="Wieland Hoffmann",
+        author_email="themineo@gmail.com",
+        scripts=["run-lala.py"],
+        package_dir = {"lala": "lala"},
+        requires=["lurklib"],
+        download_url=["https://github.com/mineo/lala/tarball/master"],
+        url=["http://github.com/mineo/lala"],
+        license="MIT",
+        classifiers=["Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: End Users/Desktop",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2.7"]
+        )
