8a681285d8d6cf4aeecb484a9bc5f8cba82d2f58
Wieland Hoffmann <themineo@gmail.com>
2011-02-16 14:19:50 +0100

    Read the config from $HOME/.lala if $XDG_CONFIG_HOME is not set

--- run-lala.py
+++ run-lala.py
@@ -8,19 +8,28 @@ from lala import Bot
 def main():
     """Main method"""
     config = ConfigParser.SafeConfigParser()
-    configfile = os.path.join(os.getenv("XDG_CONFIG_HOME"),"lala","config")
+    try:
+        configfile = os.path.join(os.getenv("XDG_CONFIG_HOME"),"lala","config")
+    except AttributeError:
+        configfile = os.path.join(os.getenv("HOME"),".lala","config")
     config.read(configfile)
     lalaconfig = config._sections["lala"]
+
     if "-d" in sys.argv:
         debug = True
     else:
         debug = False
     nickserv_password = lalaconfig["nickserv_password"] if "nickserv_password"\
             in lalaconfig else None
+
     plugins = lalaconfig["plugins"].split(",")
+    admins = []
+    for i in lalaconfig["admin"].split(","):
+        admins.append(i)
+
     bot = Bot(
             server=lalaconfig["server"],
-            admin=lalaconfig["admin"],
+            admin=admins,
             port=int(lalaconfig["port"]),
             nick=lalaconfig["nick"],
             #channel=lalaconfig["channel"],
