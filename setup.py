271d953d580396467f0a38587f7fcb3260269fe1
Wieland Hoffmann <themineo@gmail.com>
2011-03-05 10:46:58 +0100

    0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name="lala",
-        version="0.1",
+        version="0.1.1",
         author="Wieland Hoffmann",
         author_email="themineo@gmail.com",
         scripts=["run-lala.py"],
