eacd039436aa49d329a728ff456a07d59e76e546
Wieland Hoffmann <themineo@gmail.com>
2011-03-05 10:45:26 +0100

    Rewrite the decide plugin

--- lala/plugins/decide.py
+++ lala/plugins/decide.py
@@ -2,13 +2,13 @@ import logging
 from lala import plugin
 
 
-from random import randint
+from random import choice
 
 class Plugin(plugin.baseplugin):
     def __init__(self, bot):
         bot.register_callback("decide", self.decide)
 
     def decide(self, bot, user, channel, text):
-        s_text = text.split("/")[1:]
-        choice = randint(0, len(s_text)-1)
-        bot.privmsg(channel, "%s: %s" %(user, s_text[choice]))
+        s_text = text.split("/")
+        s_text[0] = " ".join(s_text[0].split()[1:])
+        bot.privmsg(channel, "%s: %s" %(user, choice(s_text)))
