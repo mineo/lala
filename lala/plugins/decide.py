b35ce6fd9b06cc6f81c009338e1d9b3bc024a4a8
Wieland Hoffmann <themineo@gmail.com>
2011-04-20 11:05:16 +0200

    plugins/decide: Fix a typo caused by vim

--- lala/plugins/decide.py
+++ lala/plugins/decide.py
@@ -5,7 +5,7 @@ from lala.util import command, msg
 
 @command
 def decide(user, channel, text):
-    """Pick one choice in an arbitrary list of choicesi separated by a slash"""
+    """Pick one choice in an arbitrary list of choices separated by a slash"""
     s_text = text.split("/")
     s_text[0] = " ".join(s_text[0].split()[1:])
     msg(channel, "%s: %s" %(user, choice(s_text)))
