6b452258148725a9e7c587ddefa5d78087c3d2fe
Wieland Hoffmann <themineo@gmail.com>
2011-08-20 16:08:48 +0200

    Add a plugin for downforeveryoneorjustme.com

---
+++ lala/plugins/down.py
@@ -0,0 +1,24 @@
+import urllib2
+import logging
+
+from lala.util import msg, command
+from httplib import HTTPException
+
+DFEOOJM_URL = "http://www.downforeveryoneorjustme.com/%s"
+
+@command 
+def isitdown(user, channel, text):
+    website = DFEOOJM_URL % " ".join(text.split()[1:])
+    logging.debug("Trying to open %s" % website)
+    try:
+        content = urllib2.urlopen(website).read()
+    except (urllib2.URLError, HTTPException), e:
+        logging.debug(e)
+        msg(channel,
+        "%s: An error occured. Please check the log for more details" % user)
+        return
+
+    if "just you" in content:
+        msg(channel, "%s: It's just you!" % user)
+    else:
+        msg(channel, "%s: It's not just you!" % user)
