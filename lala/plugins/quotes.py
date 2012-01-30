e6a1e0f24fee1a7a81717c1546dd73494764467e
Wieland Hoffmann <themineo@gmail.com>
2012-01-30 16:21:29 +0100

    use twisteds adbapi

--- lala/plugins/quotes.py
+++ lala/plugins/quotes.py
@@ -1,47 +1,55 @@
-import sqlite3
 import logging
 import os
 
 from lala.util import command, msg, on_join, is_admin
 from lala.config import get
+from twisted.enterprise import adbapi
 
 db_connection = None
 
-db_connection = sqlite3.connect(
-            os.path.join(os.path.expanduser("~/.lala"), "quotes.sqlite3"))
-db_connection.execute("CREATE TABLE IF NOT EXISTS quotes(\
+db_connection = adbapi.ConnectionPool("sqlite3",
+            os.path.join(os.path.expanduser("~/.lala"), "quotes.sqlite3"),
+            check_same_thread=False)
+db_connection.runOperation("CREATE TABLE IF NOT EXISTS quotes(\
     quote TEXT);")
-db_connection.commit()
-db_connection.text_factory = sqlite3.OptimizedUnicode
 
+def run_query(query, values, callback):
+    res = db_connection.runQuery(query, values)
+    if callback is not None:
+        res.addCallback(callback)
 
 @command
 def getquote(user, channel, text):
     """Show the quote with a specified number"""
+    def callback(quotes):
+        if len(quotes) > 0:
+            msg(channel, "[%s] %s" % (quotenumber, quotes[0][0]))
+        else:
+            msg(channel, "%s: There's no quote #%s" % (user,
+                quotenumber))
+
     s_text = text.split()
     if len(s_text) > 1:
         quotenumber = s_text[1]
         logging.debug("Trying to get quote number %s" % quotenumber)
-        with db_connection:
-            q = db_connection.execute("SELECT quote FROM quotes\
-                WHERE rowid = ?;", [quotenumber]).fetchall()
-            if len(q) > 0:
-                msg(channel, "[%s] %s" % (quotenumber, q[0][0]))
-            else:
-                msg(channel, "%s: There's no quote #%s" % (user,
-                    quotenumber))
+        run_query("SELECT quote FROM quotes WHERE rowid = ?;", 
+                [quotenumber],
+                callback)
 
 @command
 def addquote(user, channel, text):
     """Add a quote"""
+    def callback(c):
+        msg(channel, "New quote: %s" % c[0])
+
     s_text = text.split()
     if len(s_text) > 1:
         text = " ".join(s_text[1:])
         logging.debug("Adding quote: %s" % text)
-        with db_connection:
-            c = db_connection.execute("INSERT INTO quotes (quote) values (?);",
-                    [text])
-            msg(channel, "New quote: %s" % c.lastrowid)
+        run_query("INSERT INTO quotes (quote) values (?);",
+                  [text],
+                  None)
+        run_query("SELECT max(rowid) FROM quotes;", [], callback)
     else:
         msg(channel, "%s: You didn't give me any text to quote " % user)
 
@@ -53,63 +61,63 @@ def delquote(user, channel, text):
         if len(s_text) > 1:
             quotenumber = s_text[1]
             logging.debug("Deleting quote: %s" % quotenumber)
-            with db_connection:
-                db_connection.execute("DELETE FROM quotes where ROWID = (?);",
-                    [quotenumber]).fetchall()
-                db_connection.commit()
-        else:
-            msg(channel, "%s: There's no quote #%s" % (user,
-                quotenumber))
+            run_query("DELETE FROM quotes where ROWID = (?);",
+                     [quotenumber], None)
 
 @command
 def lastquote(user, channel, text):
     """Show the last quote"""
-    with db_connection:
+    def callback(quotes):
         try:
-            (id, quote) = db_connection.execute("SELECT rowid, quote FROM quotes\
-            ORDER BY rowid DESC LIMIT 1;").fetchall()[0]
+            (id, quote) = quotes[0]
+            msg(channel, "[%s] %s" % (id, quote))
         except IndexError, e:
             return
-        msg(channel, "[%s] %s" % (id, quote))
+    with db_connection:
+            run_query("SELECT rowid, quote FROM quotes ORDER BY rowid DESC\
+            LIMIT 1;", [], callback)
 
 @command
 def randomquote(user, channel, text):
     """Show a random quote"""
-    with db_connection:
+    def callback(quotes):
         try:
-            (id, quote) = db_connection.execute("SELECT rowid, quote FROM quotes ORDER\
-            BY random() LIMIT 1;").fetchall()[0]
+            (id, quote) = quotes[0]
+            msg(channel, "[%s] %s" % (id, quote))
         except IndexError, e:
             return
-        msg(channel, "[%s] %s" % (id, quote))
+
+    run_query("SELECT rowid, quote FROM quotes ORDER BY random() DESC\
+    LIMIT 1;", [], callback)
 
 @command
 def searchquote(user, channel, text):
     """Search for a quote"""
-    s_text = text.split()
-    logging.debug(s_text[1:])
-    max_quotes = int(get("max_quotes", 5))
-    with db_connection:
-        quotes = db_connection.execute("SELECT rowid, quote FROM quotes\
-        WHERE quote LIKE (?)", [
-            "".join(("%",
-                    " ".join(s_text[1:]),
-                    "%"))]
-            ).fetchall()
+    def callback(quotes):
+        max_quotes = int(get("max_quotes", 5))
         if len(quotes) > max_quotes:
             msg(channel, "Too many results, please refine your search")
         else:
             messages = ["[%s] %s" % (id, quote) for (id, quote) in quotes]
             msg(channel, messages)
 
+    s_text = text.split()
+    logging.debug(s_text[1:])
+
+    run_query(
+        "SELECT rowid, quote FROM quotes WHERE quote LIKE (?)",
+        ["".join(("%", " ".join(s_text[1:]), "%"))],
+        callback
+        )
+
 @on_join
 def join(user, channel):
-    with db_connection:
+    def callback(quotes):
         try:
-            (id, quote) = db_connection.execute("SELECT rowid, quote FROM quotes\
-                WHERE quote LIKE (?) ORDER BY random() LIMIT\
-                1;", ["".join(["%", user, "%"])]).fetchall()[0]
+            (id, quote) = quotes[0]
+            msg(channel, "[%s] %s" % (id, quote), log=False)
         except IndexError, e:
-            # There's no matching quote,
             return
-        msg(channel, "[%s] %s" % (id, quote), log=False)
+
+    run_query("SELECT rowid, quote FROM quotes where quote LIKE (?)\
+    ORDER BY random() LIMIT 1;", ["".join(["%", user, "%"])], callback)
