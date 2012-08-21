898e05a797ac7aa4478d3e0ebd7a162eac59e7ac
Wieland Hoffmann <themineo@gmail.com>
2012-08-21 15:19:28 +0200

    update the httptitle plugin to use twisted

--- lala/plugins/httptitle.py
+++ lala/plugins/httptitle.py
@@ -3,8 +3,8 @@ import logging
 import re
 import HTMLParser
 
-from httplib import HTTPException
 from lala.util import regex, msg
+from twisted.web.client import getPage
 
 _regex = re.compile("(https?://.+)\s?")
 _ua = "Mozilla/5.0 (X11; Linux x86_64; rv:2.0b8) Gecko/20100101 Firefox/4.0b8"
@@ -16,19 +16,18 @@ def unescape(s):
 @regex(_regex)
 def title(user, channel, text, match_obj):
     url = match_obj.groups()[0]
-    req = urllib2.Request(url)
-    req.add_header("User-Agent", _ua)
-    try:
-        content = urllib2.urlopen(req).read()
-    except (urllib2.URLError, HTTPException), e:
-        logging.debug("%s - %s" % (e, url))
-        return
+    def callback(content):
+        beg = content.find("<title>")
+        if beg != -1:
+            title = content[beg+7:content.find("</title>")].replace("\n","")
+            try:
+                title = unescape(title)
+            except HTMLParser.HTMLParseError, e:
+                logging.info("%s -  %s" % (e.msg, url))
+            msg(channel, "Title: %s" % unicode(title, "utf-8"))
 
-    beg = content.find("<title>")
-    if beg != -1:
-        title = content[beg+7:content.find("</title>")].replace("\n","")
-        try:
-            title = unescape(title)
-        except HTMLParser.HTMLParseError, e:
-            logging.info("%s -  %s" % (e.msg, url))
-        msg(channel, "Title: %s" % unicode(title, "utf-8"))
+    def errback(error):
+        msg(channel, "Sorry, I couldn't get the title for %s" % url)
+        logging.info(error)
+
+    getPage(str(url)).addCallbacks(callback, errback)
