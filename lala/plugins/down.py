0abc99c34bd3371a0e84e074af2d647761664478
Wieland Hoffmann <themineo@gmail.com>
2012-08-21 15:14:30 +0200

    update the down plugin to use twisted

--- lala/plugins/down.py
+++ lala/plugins/down.py
@@ -2,26 +2,25 @@ import urllib2
 import logging
 
 from lala.util import msg, command
-from httplib import HTTPException
+from twisted.web.client import getPage
 
 DFEOOJM_URL = "http://www.downforeveryoneorjustme.com/%s"
 
 @command
 def isitdown(user, channel, text):
+    def callback(content):
+        if "It's just you" in content:
+            msg(channel, "%s: It's just you!" % user)
+        else:
+            msg(channel, "%s: It's not just you!" % user)
+
+    def errback(error):
+        msg(channel, "%s: Sorry, something went wrong while processing your request." % user)
+        logging.info(error)
+
     s_text = text.split()
     if len(s_text) <= 1:
         return
     website = DFEOOJM_URL % " ".join(s_text[1:])
     logging.debug("Trying to open %s" % website)
-    try:
-        content = urllib2.urlopen(website).read()
-    except (urllib2.URLError, HTTPException), e:
-        logging.debug(e)
-        msg(channel,
-        "%s: An error occured. Please check the log for more details" % user)
-        return
-
-    if "It's just you" in content:
-        msg(channel, "%s: It's just you!" % user)
-    else:
-        msg(channel, "%s: It's not just you!" % user)
+    getPage(str(website)).addCallbacks(callback, errback)
