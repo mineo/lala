bc285770541b4746112a6bdf512b9723011cf37a
Wieland Hoffmann <themineo@gmail.com>
2012-12-14 16:08:51 +0100

    update the version on 'build' as well

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import os, subprocess, re
 from distutils.core import setup, Command
 from distutils.command.sdist import sdist as _sdist
+from distutils.command.build import build as _build
 
 # The following code is taken from
 # https://github.com/warner/python-ecdsa/blob/f03abf93968019758c6e00753d1b34b87fecd27e/setup.py
@@ -73,6 +74,15 @@ class sdist(_sdist):
         self.distribution.metadata.version = get_version()
         return _sdist.run(self)
 
+
+class build(_build):
+    def run(self):
+        update_version_py()
+        # unless we update this, the build command will keep using the old
+        # version
+        self.distribution.metadata.version = get_version()
+        return _build.run(self)
+
 # Here ends the code taken from Brian Warner
 
 setup(name="lala",
@@ -95,5 +105,5 @@ setup(name="lala",
             "Programming Language :: Python :: 2.6",
             "Programming Language :: Python :: 2.7"],
         data_files=[("usr/share/doc/lala", ["config.example"])],
-        cmdclass={"version": Version, "sdist": sdist}
+        cmdclass={"version": Version, "sdist": sdist, "build": build}
         )
