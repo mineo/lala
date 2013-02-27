b3ef748df9eca585ae3fc77da666ba5ce93bc428
Wieland Hoffmann <themineo@gmail.com>
2013-02-27 19:04:38 +0100

    Replace newlines with spaces for readability

--- lala/plugins/fortune.py
+++ lala/plugins/fortune.py
@@ -25,7 +25,7 @@ def _call_fortune(user, channel, args=[]):
     deferred.addErrback(logging.error)
 
 def _send_output_to_channel(user, channel, text):
-    msg(channel, "%s: %s" %(user, text.replace("\n","")))
+    msg(channel, "%s: %s" %(user, text.replace("\n"," ")))
 
 def _send_error_to_channel(user, channel, exception):
     msg(channel, "%s: Sorry, no fortune for you today! Details are in the log." % user)
