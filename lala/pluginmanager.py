d92ff3259e7139b2c760b477a1f698e576ddcf78
Wieland Hoffmann <themineo@gmail.com>
2014-01-24 12:00:53 +0100

    pep8

--- lala/pluginmanager.py
+++ lala/pluginmanager.py
@@ -9,8 +9,10 @@ _join_callbacks = list()
 _regexes = {}
 _cbprefix = "!"
 
+
 def _make_pluginfunc(func, admin_only=False):
     return {'enabled': True, 'func': func, 'admin_only': admin_only}
+
 
 def is_admin(user):
     """Check whether ``user`` is an admin.
@@ -18,13 +20,14 @@ def is_admin(user):
     If a nickserv password is set, this will work by checking an internal
     list of identified admins.
 
-    If not nickserv password is set, this simply checks if ``user`` is in
+    If no nickserv password is set, this simply checks if ``user`` is in
     the "admins" option of the "base" section."""
     if lala.util._BOT.factory.nspassword is not None:
         return user in lala.util._BOT.identified_admins
     else:
         return user in lala.config._get("base",
                                         "admins").split(lala.config._LIST_SEPARATOR)
+
 
 def load_plugin(name):
     logging.debug("Trying to load %s" % name)
@@ -34,19 +37,23 @@ def load_plugin(name):
     (f, p, d) = imp.find_module(name)
     imp.load_module(name, f, p, d)
 
+
 def register_callback(trigger, func, admin_only=False):
     """ Adds ``func`` to the callbacks for ``trigger``."""
     logging.debug("Registering callback for %s" % trigger)
     _callbacks[trigger] = _make_pluginfunc(func, admin_only)
 
+
 def register_join_callback(func):
     """ Registers ``func`` as a callback for join events."""
     _join_callbacks.append(func)
+
 
 def register_regex(regex, func):
     """ Registers ``func`` as a callback for every message that matches
     ``regex``."""
     _regexes[regex] = _make_pluginfunc(func)
+
 
 def _handle_message(user, channel, message):
     if message.startswith(_cbprefix):
@@ -83,12 +90,14 @@ def _handle_message(user, channel, message):
             else:
                 logging.info("%s is not enabled" % regex.pattern)
 
+
 def on_join(user, channel):
     """ Calls all callbacks for on_join events that were previously
     registered with :meth:`lala.util.on_join`.
     """
     for cb in _join_callbacks:
         cb(user, channel)
+
 
 def disable(trigger):
     """Disables `trigger`.
@@ -104,6 +113,7 @@ def disable(trigger):
             _regexes[regex]["enabled"] = False
             break
 
+
 def enable(trigger):
     """Enables `trigger`.
 
@@ -118,11 +128,13 @@ def enable(trigger):
         if regex.pattern == trigger:
             _regexes[regex]["enabled"] = True
 
+
 def _get_enabled_plugins():
     """Returns a list of all the enabled plugins.
     """
     return lala.config._get("base",
-            "plugins").split(lala.config._LIST_SEPARATOR)
+                            "plugins").split(lala.config._LIST_SEPARATOR)
+
 
 def _reload():
     """Reloads all enabled plugins.
@@ -136,6 +148,7 @@ def _reload():
     # already imported module, the second import is equivalent to a reload() of
     # that module.
     setup()
+
 
 def setup():
     """Loads all enabled plugins
