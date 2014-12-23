a617a1469b03d67cfc8d0a7ae4f9a1b45dff2102
Wieland Hoffmann <themineo@gmail.com>
2014-12-23 18:52:21 +0100

    httptitle: Use inlineCallbacks

--- lala/plugins/httptitle.py
+++ lala/plugins/httptitle.py
@@ -4,29 +4,33 @@ import HTMLParser
 
 from lala.util import regex, msg
 from twisted.web.client import getPage
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 _regex = re.compile("(https?://.+)\s?")
 _ua = "Mozilla/5.0 (X11; Linux x86_64; rv:2.0b8) Gecko/20100101 Firefox/4.0b8"
+
 
 def unescape(s):
     p = HTMLParser.HTMLParser()
     return p.unescape(s)
 
+
 @regex(_regex)
+@inlineCallbacks
 def title(user, channel, text, match_obj):
     url = match_obj.groups()[0]
-    def callback(content):
-        beg = content.find("<title>")
+
+    try:
+        page = yield getPage(str(url))
+        beg = page.find("<title>")
         if beg != -1:
-            title = content[beg+7:content.find("</title>")].replace("\n","")
+            title = page[beg + 7:page.find("</title>")].replace("\n", "")
             try:
                 title = unescape(title)
             except HTMLParser.HTMLParseError, e:
                 logging.exception("%s -  %s" % (e.msg, url))
             msg(channel, "Title: %s" % unicode(title, "utf-8"))
 
-    def errback(error):
+    except Exception as exc:
         msg(channel, "Sorry, I couldn't get the title for %s" % url)
-        return error
-
-    getPage(str(url)).addCallbacks(callback, errback)
+        returnValue(exc)
