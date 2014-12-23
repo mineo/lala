ae43873d435ce3e70b90ee844187a87d31eb0a3c
Wieland Hoffmann <themineo@gmail.com>
2014-12-23 18:58:12 +0100

    down: Use inlineCallbacks

--- lala/plugins/down.py
+++ lala/plugins/down.py
@@ -1,25 +1,23 @@
-import urllib2
 import logging
 
 
 from lala.util import msg, command
 from twisted.web.client import getPage
+from twisted.internet.defer import inlineCallbacks
 
 DFEOOJM_URL = "http://www.downforeveryoneorjustme.com/%s"
 
-@command
-def isitdown(user, channel, text):
-    def callback(content):
-        if "It's just you" in content:
-            msg(channel, "%s: It's just you!" % user)
-        else:
-            msg(channel, "%s: It's not just you!" % user)
 
+@command
+@inlineCallbacks
+def isitdown(user, channel, text):
     s_text = text.split()
     if len(s_text) <= 1:
         return
     website = DFEOOJM_URL % " ".join(s_text[1:])
     logging.debug("Trying to open %s" % website)
-    d = getPage(str(website))
-    d.addCallback(callback)
-    return d
+    content = yield getPage(str(website))
+    if "It's just you" in content:
+        msg(channel, "%s: It's just you!" % user)
+    else:
+        msg(channel, "%s: It's not just you!" % user)
