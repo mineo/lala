bb1d6bde67290832515a778ebcff3b83ba0e0b72
Wieland Hoffmann <themineo@gmail.com>
2014-12-30 02:02:24 +0100

    decide: Deal with changes made in f9ccf06

--- lala/plugins/decide.py
+++ lala/plugins/decide.py
@@ -6,5 +6,4 @@ from lala.util import command, msg
 def decide(user, channel, text):
     """Pick one choice in an arbitrary list of choices separated by a slash"""
     s_text = text.split("/")
-    s_text[0] = " ".join(s_text[0].split()[1:])
     msg(channel, "%s: %s" % (user, choice(s_text)))
