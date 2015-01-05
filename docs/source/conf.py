e6dbf7621a41d8835223ec366f096f2bc18d28b3
Wieland Hoffmann <themineo@gmail.com>
2015-01-05 17:25:21 +0100

    It's 2015

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'lala'
-copyright = u'2011-2014, Wieland Hoffmann'
+copyright = u'2011-2015, Wieland Hoffmann'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
