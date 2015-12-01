d811cc90a5c2eab4167c516904ce27a1a522892b
Wieland Hoffmann <themineo@gmail.com>
2015-12-01 11:56:23 +0100

    pep8

--- lala/plugins/decide.py
+++ lala/plugins/decide.py
@@ -16,15 +16,18 @@ from lala.util import command, msg
 
 TRIES = 5000
 
+
 @command
 def decide(user, channel, text):
     """Pick one choice in an arbitrary list of choices separated by a slash"""
     s_text = text.split("/")
     msg(channel, "%s: %s" % (user, choice(s_text)))
 
+
 @command
 def decide_real_hard(user, channel, text):
-    """Pick one choice in an arbitrary list of choices separated by a slash, deluxe version"""
+    """Pick one choice in an arbitrary list of choices separated by a slash,
+    deluxe version"""
     s_text = text.split("/")
     c = Counter(choice(s_text) for i in range(TRIES)).most_common(1)[0]
     msg(channel, "%s: %s has been chosen %i out of %i times" %
